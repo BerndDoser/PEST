c0f0f98c9af625675f79380f188d98c7eac6781f
Bernd Doser <bernd.doser@h-its.org>
2025-05-26 16:56:43 +0200

    work on IllustrisDownloader

--- src/pest/__init__.py
+++ src/pest/__init__.py
@@ -2,7 +2,7 @@ import importlib.metadata
 
 from .fits_converter import FitsConverter
 from .gaia_converter import GaiaConverter
-from .illustris_downloader import IllustrisDownloader
+from .illustris_downloader import IllustrisDownloader, PropertyType, Selector
 from .illustris_preprocess_api import data_preprocess_api
 from .illustris_preprocess_local import data_preprocess_local
 
@@ -10,7 +10,9 @@ __version__ = importlib.metadata.version("pest")
 __all__ = [
     "data_preprocess_api",
     "data_preprocess_local",
-    "GaiaConverter",
     "FitsConverter",
+    "GaiaConverter",
     "IllustrisDownloader",
+    "PropertyType",
+    "Selector",
 ]
--- src/pest/illustris_downloader.py
+++ src/pest/illustris_downloader.py
@@ -1,15 +1,30 @@
 import os
+import time
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 import requests
 
 from pest.get_illustris_apy_key import get_illustris_api_key
 
 
+class PropertyType(Enum):
+    MASS = "mass"
+
+
+@dataclass
+class Selector:
+    property: PropertyType
+    min_value: float
+    max_value: float
+
+
 class IllustrisDownloader(object):
+
     def __init__(
         self,
-        download_path: str = "data",
-        base_path: str = "http://www.tng-project.org/api",
+        base_url: str = "http://www.tng-project.org/api",
         simulation: str = "TNG100-1",
         snapshot: int = 99,
         timeout: int = 100,
@@ -21,17 +36,52 @@ class IllustrisDownloader(object):
             snap_num (int, optional): Defaults to 99.
 
         """
-        self.download_path = download_path
         self.headers = {"api-key": get_illustris_api_key()}
-        self.url = os.path.join(base_path, simulation, "snapshots", str(snapshot))
+        self.url = os.path.join(base_url, simulation, "snapshots", str(snapshot))
         self.params = {
             "stars": "Coordinates,Masses",
             "gas": "Coordinates,Potential",
         }
         self.timeout = timeout
 
+    def get_subhalos(
+        self,
+        limit: Optional[int] = None,
+        selector: Optional[Selector] = None,
+    ) -> dict:
+        """Get subhalo ids from the Illustris API."""
+
+        params = {}
+        if limit is not None:
+            params["limit"] = limit
+
+        search_query = ""
+        if selector is not None:
+            search_query = (
+                f"?{selector.property}__gt="
+                + str(selector.min_value)
+                + f"&{selector.property}__lt="
+                + str(selector.max_value)
+            )
+
+        print(search_query)
+
+        start_time = time.time()
+        subhalos = requests.get(
+            url=os.path.join(self.url, "subhalos", search_query),
+            headers=self.headers,
+            params=params,
+            timeout=self.timeout,
+        )
+        print(f"Request time: {time.time() - start_time:.2f} seconds")
+
+        subhalos.raise_for_status()
+        if subhalos.headers["content-type"] != "application/json":
+            raise RuntimeError("Response content is not JSON")
+
+        return subhalos.json()
+
     def get(self, subhalo_id: int):
-        # make HTTP GET request to path
         r = requests.get(
             url=os.path.join(self.url, "subhalos", str(subhalo_id)),
             headers=self.headers,
@@ -46,22 +96,22 @@ class IllustrisDownloader(object):
 
         return r.json()
 
-    def get_hdf5(self, subhalo_id: int):
-        # make HTTP GET request to path
-        r = requests.get(
-            url=os.path.join(self.url, "subhalos", str(subhalo_id), "cutout.hdf5"),
-            headers=self.headers,
-            params=self.params,
-            timeout=self.timeout,
-        )
+    # def get_hdf5(self, subhalo_id: int):
+    #     # make HTTP GET request to path
+    #     r = requests.get(
+    #         url=os.path.join(self.url, "subhalos", str(subhalo_id), "cutout.hdf5"),
+    #         headers=self.headers,
+    #         params=self.params,
+    #         timeout=self.timeout,
+    #     )
 
-        # raise exception if response code is not HTTP SUCCESS (200)
-        r.raise_for_status()
+    #     # raise exception if response code is not HTTP SUCCESS (200)
+    #     r.raise_for_status()
 
-        if "content-disposition" not in r.headers:
-            raise RuntimeError("No content-disposition header found")
+    #     if "content-disposition" not in r.headers:
+    #         raise RuntimeError("No content-disposition header found")
 
-        filename = r.headers["content-disposition"].split("filename=")[1]
-        with open(os.path.join(self.download_path, filename), "wb") as f:
-            f.write(r.content)
-        return filename
+    #     filename = r.headers["content-disposition"].split("filename=")[1]
+    #     with open(os.path.join(self.download_path, filename), "wb") as f:
+    #         f.write(r.content)
+    #     return filename
