5026d7800af6ae9ef531d0a7e2e0be2e2a0cb426
Bernd Doser <bernd.doser@h-its.org>
2025-05-28 11:07:46 +0200

    update file path for Illustris API key to project root directory

--- src/pest/get_illustris_apy_key.py
+++ src/pest/get_illustris_apy_key.py
@@ -4,10 +4,13 @@ import os
 def get_illustris_api_key():
     """Return the API key for the Illustris API.
     The key can be set as an environment variable or in a file."""
+    api_file = os.path.join(
+        os.path.dirname(__file__), "..", "..", ".illustris_api_key.txt"
+    )
     if "ILLUSTRIS_API_KEY" in os.environ:
         return os.environ["ILLUSTRIS_API_KEY"]
-    elif os.path.isfile(".illustris_api_key.txt"):
-        with open(".illustris_api_key.txt", "r") as file:
+    elif os.path.isfile(api_file):
+        with open(api_file, "r") as file:
             return file.read().rstrip()
     raise ValueError(
         "No API key found. Please set the ILLUSTRIS_API_KEY environment variable"
