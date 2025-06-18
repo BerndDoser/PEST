8ef6f5e28cd76a99cedc9c382fdd326bdf28a2a1
Bernd Doser <bernd.doser@h-its.org>
2025-06-18 12:18:46 +0200

    ruff reformat

--- src/pest/get_illustris_apy_key.py
+++ src/pest/get_illustris_apy_key.py
@@ -4,9 +4,7 @@ import os
 def get_illustris_api_key():
     """Return the API key for the Illustris API.
     The key can be set as an environment variable or in a file."""
-    api_file = os.path.join(
-        os.path.dirname(__file__), "..", "..", ".illustris_api_key.txt"
-    )
+    api_file = os.path.join(os.path.dirname(__file__), "..", "..", ".illustris_api_key.txt")
     if "ILLUSTRIS_API_KEY" in os.environ:
         return os.environ["ILLUSTRIS_API_KEY"]
     elif os.path.isfile(api_file):
