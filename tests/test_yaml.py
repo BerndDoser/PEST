051caa7102ec041d0f8f5752f3e2a0f56cd4df19
Bernd Doser <bernd.doser@h-its.org>
2025-07-14 16:44:44 +0200

    correct field structure in config.yaml

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -5,4 +5,4 @@ def test_yaml():
     with open("tests/config.yaml", "rb") as f:
         conf = yaml.safe_load(f.read())
 
-    assert conf["field"] == "masses"
+    assert conf["source"]["init_args"]["component"][0]["fields"][0] == "masses"
