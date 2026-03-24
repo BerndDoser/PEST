6f5673f9b8d25fb11fb14ff0d93174592224fb23
Bernd Doser <bernd.doser@h-its.org>
2026-03-24 10:21:18 +0100

    update image size and output path in FitsConverter

--- devel/convert_fits_all.py
+++ devel/convert_fits_all.py
@@ -1,8 +1,9 @@
 from pest import FitsConverter
 
 FitsConverter(
-    image_size=512,
+    image_size=128,
     datatype="png",
+    chunk_size=10000,
 ).convert_all(
     [
         "/hits/basement/its/doserbd/data/SKIRT_synthetic_images/fits/TNG100/sdss/snapnum_099/data",
@@ -12,5 +13,5 @@ FitsConverter(
         "/hits/basement/its/doserbd/data/SKIRT_synthetic_images/fits/Illustris/sdss/snapnum_135/data",
         "/hits/basement/its/doserbd/data/SKIRT_synthetic_images/fits/Illustris/sdss/snapnum_131/data",
     ],
-    "/hits/basement/its/doserbd/data/SKIRT_synthetic_images/parquet-v5-512",
+    "/urz/gpuscratch/its/doserbd/data/SKIRT_synthetic_images/parquet-v5-128-png",
 )
