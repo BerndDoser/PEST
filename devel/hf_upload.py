1aa34f28d582132ff5f3d77aa9560e3801a7e79f
Bernd Doser <bernd.doser@h-its.org>
2026-04-28 23:00:50 +0200

    hf upload

---
+++ devel/hf_upload.py
@@ -0,0 +1,4 @@
+from datasets import Dataset
+
+dataset = Dataset.from_parquet("/urz/gpuscratch/its/doserbd/data/SKIRT_synthetic_images/parquet-v4-128")
+dataset.push_to_hub("bernddoser/illustris-skirt", max_shard_size="500MB")
