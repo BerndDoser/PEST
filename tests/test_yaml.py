aed03221c8b89c379382661c734206ed2cffb6b5
Bernd Doser <bernd.doser@h-its.org>
2026-05-07 11:22:04 +0200

    remove deprecated yaml test

--- tests/test_yaml.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-def test_yaml():
-    with open("tests/config.yaml", "rb") as f:
-        conf = yaml.safe_load(f.read())
-
-    assert conf["source"]["init_args"]["component"][0]["fields"][0] == "masses"
